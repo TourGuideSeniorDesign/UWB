0c766a83d42cc344bd2dba52a7ae153ce862c08e
Eren Tekbas <tekbasa@lafayette.edu>
2025-04-28 14:32:54 -0400

    microRos publisher for UWB initial draft

--- UWBtransmitter/src/microRosFunctions.cpp
+++ UWBtransmitter/src/microRosFunctions.cpp
@@ -1,5 +1,7 @@
-#if defined(ROS) || defined(ROS_DEBUG)
+/* MicroROS Publisher code for the UWB transmitter
+*/
 
+#if defined(ROS) || defined(ROS_DEBUG)
 #include "dw3000Functions.h"
 #include "Arduino.h"
 #include "DW3000.h"
@@ -18,31 +20,19 @@
 // ROS messages
 #ifdef ROS_DEBUG
 #include <wheelchair_sensor_msgs/msg/UWB.h>
-
-#elif ROS
-
 #endif
 
 // Executor
 rclc_executor_t executor;
 
 // Subscriber
-rcl_subscription_t subscriber;
-wheelchair_sensor_msgs__msg__UWB UWBdistance;
-
-
-rcl_subscription_t uwb_subscriber;
-wheelchair_sensor_msgs__msg__UWB UWBdistance;
+/* rcl_subscription_t subscriber;
+wheelchair_sensor_msgs__msg__UWB UWBdistance; */
 
 // UWB publisher and timer
 rcl_publisher_t uwb_Publisher;
 rcl_timer_t uwbTimer;
 wheelchair_sensor_msgs__msg__UWB UWBdistance;
-
-
-#ifdef ROS_DEBUG
-
-#endif
 
 rclc_support_t support;
 rcl_allocator_t allocator;
@@ -53,6 +43,9 @@ rcl_timer_t timer;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){} }
 
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){} }
+
 // Error handle loop
 void error_loop() {
     while(1) {
@@ -60,19 +53,58 @@ void error_loop() {
     }
 }
 
-// Callback to trigger TWR and publish result
-void uwb_timer_callback(rcl_timer_t *input_timer, int64_t last_call_time) {
+// Create Entities
+void createEntities(const char* nodeName, const char* pubTopicName, uint16_t timer_interval_ms) {
+    allocator = rcl_get_default_allocator();
+    set_microros_serial_transports(Serial);
+    delay(2000);
+
+    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+    RCCHECK(rclc_node_init_default(&node, nodeName, "", &support));
+
+    RCCHECK(rclc_publisher_init_default(
+        &uwbPublisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, UWB),
+        pubTopicName));
+
+    RCCHECK(rclc_timer_init_default(
+        &uwbTimer,
+        &support,
+        RCL_MS_TO_NS(timer_interval_ms),
+        uwb_timer_callback));
+
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &uwbTimer));
+}
+
+// Destroy Entities
+void destroyEntities() {
+    rcl_publisher_fini(&uwbPublisher, &node);
+    rcl_timer_fini(&uwbTimer);
+    rcl_node_fini(&node);
+    rclc_executor_fini(&executor);
+    rclc_support_fini(&support);
+}
+
+// Timer callback to publish UWB message
+void uwb_timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
     RCLC_UNUSED(last_call_time);
-    if (input_timer == NULL) return;
+    if (timer == NULL) return;
 
     for (int i = 0; i < MAX_RESPONDERS; i++) {
-        float distance = rangingManager.rangeWithResponder(i);
+        float distance; // get the actual reading from the sensor
         if (distance > 0.0f) {
-            uwbMsg.responder_id = i;
-            uwbMsg.distance_m = distance;
-            RCSOFTCHECK(rcl_publish(&uwbPublisher, &uwbMsg, NULL));
+            switch(i) {
+                case 0: uwbMsg.distance1 = distance; break;
+                case 1: uwbMsg.distance2 = distance; break;
+                case 2: uwbMsg.distance3 = distance; break;
+                case 3: uwbMsg.distance4 = distance; break;
+                case 4: uwbMsg.distance5 = distance; break;
+            }
         }
     }
+    RCSOFTCHECK(rcl_publish(&uwbPublisher, &uwbMsg, NULL));
 }
 
 #ifdef ROS_DEBUG
@@ -82,148 +114,31 @@ void timer_callback(rcl_timer_t * inputTimer, int64_t last_call_time) {
         RCSOFTCHECK(rcl_publish(&uwbPublisher, &uwbMsg, NULL));
     }
 }
+#endif
 
-
-void subscription_callback(const void *msgin)
-{
-    const wheelchair_sensor_msgs__msg__RefSpeed *msg = (const wheelchair_sensor_msgs__msg__RefSpeed *)msgin;
-    refSpeedMsg = *msg;
-#ifdef ROS_DEBUG
-
-
-// UWB setup
-void uwbSetup(uint16_t timer_interval_ms, const char* pubTopicName) {
-    if (!DW3000.begin()) {
-        error_loop();  // Failed to initialize
-    }
-
-    rangingManager.init(&DW3000);
-
-    // ROS: Init publisher
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, UwbDistance),
-        pubTopicName));
-
-    // Timer for TWR loop
-    RCCHECK(rclc_timer_init_default(
-        &uwbTimer,
-        &support,
-        RCL_MS_TO_NS(timer_interval_ms),
-        uwb_timer_callback));
-
-    // Add to executor
-    RCCHECK(rclc_executor_add_timer(&executor, &uwbTimer));
+// Loop Tick
+void microRosTick() {
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 }
 
-#endif
 
-
-    void microRosSetup(unsigned int timer_timeout, const char* nodeName, const char* subTopicName, const char* pubTopicName){
-
-    set_microros_serial_transports(Serial);
-    delay(2000);
-    allocator = rcl_get_default_allocator();
-
-    const size_t domain_id = 7;
-    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-    RCCHECK(rclc_node_init_default(&node, nodeName, "", &support));
-
-    // create uwb subscriber
-    RCCHECK(rclc_subscription_init_best_effort(
-            &uwb_subscriber1,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance1),
-            "distance1"));
-
-    RCCHECK(rclc_subscription_init_best_effort(
-            &uwb_subscriber2,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance2),
-            "distance2"));
-
-    RCCHECK(rclc_subscription_init_best_effort(
-            &uwb_subscriber3,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance3),
-            "distance3"));
-
-    RCCHECK(rclc_subscription_init_best_effort(
-            &uwb_subscriber4,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance4),
-            "distance4"));
-    RCCHECK(rclc_subscription_init_best_effort(
-            &uwb_subscriber4,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance5),
-            "distance5"));
-
-
-    // uwb: init publisher and timer
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher1,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance1),
-        "uwb_distance1"));
-
-    // uwb: init publisher and timer
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher2,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance2),
-        "uwb_distance2"));
-
-    // uwb: init publisher and timer
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher3,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance3),
-        "uwb_distance3"));
-
-    // uwb: init publisher and timer
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher4,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance4),
-        "uwb_distance4"));
-
-    // uwb: init publisher and timer
-    RCCHECK(rclc_publisher_init_default(
-        &uwbPublisher4,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, distance5),
-        "uwb_distance5"));
-
-
-    RCCHECK(rclc_timer_init_default(
-        &uwbTimer,
-        &support,
-        RCL_MS_TO_NS(1000)
-        uwb_timer_callback));
-
-
-#ifdef ROS
-
-
-#elif ROS_DEBUG
-
-    RCCHECK(rclc_publisher_init_best_effort(
-        &uwbPublisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(wheelchair_sensor_msgs, msg, uwbValues),
-        "uwb_value"));
-
-
-#endif
-
+void subscription_callback(const void *msgin){
+    const wheelchair_sensor_msgs__msg__UWB *msg = (const wheelchair_sensor_msgs__msg__UWB *)msgin;
+    UWBdistance= *msg;
 #ifdef ROS_DEBUG
-    RCCHECK(rclc_timer_init_default(
-        &timer,
-        &support,
-        RCL_MS_TO_NS(timer_timeout),
-        timer_callback));
+#elif ROS
 #endif
+}
+
+
+UWBdistance getUWBDistance(){
+  UWBdistance d;
+  d.distance1=uwbMsg.distance1;
+  d.distance2=uwbMsg.distance2;
+  d.distance3=uwbMsg.distance3;
+  d.distance4=uwbMsg.distance4;
+  d.distance5=uwbMsg.distance5;
+  return d;
+}
 
 #endif
